2f9974ead6b831ec497e97f6a3d9138e8153da50
choudharysidharth082000 <choudharysidharth082000@gmail.com>
2022-09-01 14:25:02 +0530

    Sample

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div className="pSide w-[30%] h-screen bg-white">
+    <div className="Side w-[30%] h-screen bg-white">
       {/* Logo Part */}
       <div className="logoContainor d-flex w-full p-2 flex justify-center items-center mt-2">
         <img className='object-contain w-28' src="https://zeeqtina.netlify.app/_next/static/media/logo.fe1ffe5c.svg" alt="" />        
